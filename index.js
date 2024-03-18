65acbad22a85fdbd30b64205d20551ea62b76772
Rosshoven <Michaeledwardross@gmail.com>
2024-03-18 16:36:17 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Calling the root file or "startup" file index.js
+// import express from 'express';  
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
